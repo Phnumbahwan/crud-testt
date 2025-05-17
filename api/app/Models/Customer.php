3f8a6642bd906bd010b4d5b5cb30ffd672bd1907
Gil Pacturan <gilpacturan@gmail.com>
2025-05-17 20:37:37 +0800

    Add booted function to my model to index all saved data

--- api/app/Models/Customer.php
+++ api/app/Models/Customer.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Services\ElasticsearchService;
 
 class Customer extends Model
 {
@@ -21,13 +22,14 @@ class Customer extends Model
         'contact_number'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+    protected static function booted()
+    {
+        static::saved(function ($customer) {
+            app(ElasticsearchService::class)->create('customers', $customer->id, $customer->only('first_name', 'last_name', 'email', 'contact_number'));
+        });
+
+        static::deleted(function ($customer) {
+            app(ElasticsearchService::class)->delete('customers', $customer->id);
+        });
+    }
 }
