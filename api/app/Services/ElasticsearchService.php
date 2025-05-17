b036275d2544c7656c461648eb9e97be51ddec84
Gil Pacturan <gilpacturan@gmail.com>
2025-05-17 21:36:18 +0800

    Already have a model booted code to handle elasticsearch update

--- api/app/Services/ElasticsearchService.php
+++ api/app/Services/ElasticsearchService.php
@@ -13,16 +13,6 @@ class ElasticsearchService
         $this->host = config('services.elasticsearch.host');
     }
 
-    public function create(string $index, string $id, array $data): array
-    {
-        return Http::put("{$this->host}/{$index}/_doc/{$id}", $data)->json();
-    }
-
-    public function delete(string $index, string $id): array
-    {
-        return Http::delete("{$this->host}/{$index}/_doc/{$id}")->json();
-    }
-
     public function search(string $index, array $query): array
     {
         return Http::post("{$this->host}/{$index}/_search", $query)->json();
