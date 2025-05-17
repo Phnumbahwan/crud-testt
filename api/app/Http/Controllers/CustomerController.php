2646ce8184edf15c0ba69049950af7f6fd561922
Gil Pacturan <gilpacturan@gmail.com>
2025-05-17 20:38:25 +0800

    Create list all customers controller

--- api/app/Http/Controllers/CustomerController.php
+++ api/app/Http/Controllers/CustomerController.php
@@ -2,18 +2,55 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Pagination\LengthAwarePaginator;
 use App\Http\Requests\StoreCustomerRequest;
 use App\Http\Requests\UpdateCustomerRequest;
 use App\Models\Customer;
+use App\Services\ElasticsearchService;
+use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request, ElasticsearchService $es)
     {
-        return Customer::paginate();
+        $perPage = 15;
+        $search = $request->query('search');
+        $page = $request->query('page', 1);
+
+        if ($search) {
+            $query = [
+                'from' => ($page - 1) * $perPage,
+                'size' => $perPage,
+                'query' => [
+                    'multi_match' => [
+                        'query' => $search,
+                        'fields' => ['first_name', 'last_name', 'email']
+                    ]
+                ]
+            ];
+
+            $response = $es->search('customers', $query);
+            $hits = collect($response['hits']['hits']);
+            $ids = $hits->pluck('_id');
+            $total = $response['hits']['total']['value'];
+
+            $customers = Customer::whereIn('id', $ids)->get()
+                ->sortBy(fn($model) => array_search($model->id, $ids->all()))
+                ->values();
+
+            return new LengthAwarePaginator(
+                $customers,
+                $total,
+                $perPage,
+                $page,
+                ['path' => url()->current(), 'query' => $request->query()]
+            );
+        }
+
+        return Customer::paginate($perPage);
     }
 
     /**
